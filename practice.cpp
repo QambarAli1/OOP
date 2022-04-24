7d716fc56c10f4f218dbf3f94e53d81b7496464f
unknown <qamberali4904@gmail.com>
2022-04-24 16:57:59 +0500

    updated

--- practice.cpp
+++ practice.cpp
@@ -21,8 +21,64 @@ class Student{
             };
         };
 };
+
+// Recursion Function
+int factorial(int n){
+    if(n<=1){
+        return 1;
+    }
+    return n * factorial(n-1);
+};
+
+void derefrenPoin(){
+    //de-referencing pointer
+    int x = 5;
+    int* y = &x;
+    cout<<"value of x = "<<x<<endl;
+    cout<<"address of y = "<<y<<endl;
+    cout<<"value of y = "<<*y<<endl;
+}
+
 int main(){
-    Student Student1("Qambar",57,3.90);
-    cout<<Student1.Eligible()<<endl;
-    return 0;
+    // Student Student1("Qambar",57,3.90);
+    // cout<<Student1.Eligible()<<endl;
+
+    // Recursion Function
+
+    // int a;
+    // cout<<"Enter a number "<<endl;
+    // cin>>a;
+    // cout<<"Factorial of "<< a << " is "<<factorial(a)<<endl;
+    // return 0;
+
+
+    // char car[10] = "car array";
+    // char* pointerCar = car; //stores first char address
+    // cout<<"Pointer Car Array = "<< &pointerCar <<endl;
+    // *pointerCar = 'j'; //changes first char by using pointer
+    // cout<<"Pointer Car Array = "<< pointerCar <<endl;
+
+
+    // char a[5] =  "ali";
+    // char b[5] = "aun";
+    // char* addB = b;
+    // char* c = "elia";
+    // char* d = "ali";
+    // // d="ali";
+    // // b="xli"; // does not work
+    // c=d; // this works
+    // cout<<"a : "<<a<<endl;
+    // cout<<"b : "<<b<<endl;
+    // cout<<"c : "<<c<<endl;
+    // cout<<"d : "<<d<<endl;
+
+    
+    // derefrenPoin();
+
+
+    
+
+
+
+
 };
